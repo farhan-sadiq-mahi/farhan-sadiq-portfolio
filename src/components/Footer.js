beae1d92065efed92f24eaeeada725035cff80ba
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-12-08 00:02:19 +0600

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer pt-5">
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
